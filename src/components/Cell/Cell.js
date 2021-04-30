bcfcc5be219db5702b498d5e59222d12ad1662bb
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-04-30 16:39:24 +0300

    added inputs and checkboxes

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './Cell.css';
 
 export function Cell ({ cell, rowIndex, cellIndex }) {
+  const [isChecked, setIsChecked] = useState(false);
+  const [content, setContent] = useState('');
 
+  function setNewContent(content) {
+    if (Number.isInteger(+content)) {
+      setContent(parseInt(content));
+    };
+
+    if (content === '') {
+      setContent(content);
+    };
+  };
 
   return (
-
     <div
       className={
         cellIndex === 0 && rowIndex !== 0
@@ -16,10 +27,38 @@ export function Cell ({ cell, rowIndex, cellIndex }) {
         : 'cell'
       }
     >
-
       {cellIndex === 0 ? (rowIndex !== 0 ? rowIndex : '') : ''}
       {rowIndex === 0 ? (cellIndex !== 0 ? cellIndex : '') : ''}
 
+
+      {(cellIndex !== 0 && rowIndex !== 0) &&
+        <>
+          <label>
+
+            <input
+              type="checkbox"
+              className={'checkbox'}
+              checked={isChecked}
+              onChange={() => setIsChecked(prev => !prev)}
+            >
+            </input>
+
+            <input
+              type="text"
+              className={'textInput'}
+              value={content}
+              onChange={event => setNewContent(event.target.value)}
+              disabled={isChecked ? true : false}
+            >
+            </input>
+
+            <div className={'fakeCheckbox'}>
+              {isChecked && 'X'}
+            </div>
+
+          </label>
+        </>
+      }
     </div>
   );
-}
+};
